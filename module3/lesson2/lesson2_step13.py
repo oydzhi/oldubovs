d23cbf950e4b8626cbe8094cf0323a4a50a5b01e
oydzhi <115539162+oydzhi@users.noreply.github.com>
2025-04-09 15:21:05 +0500

    New added files after lessons

---
+++ module3/lesson2/lesson2_step13.py
@@ -0,0 +1,55 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestCases(unittest.TestCase):
+    def test_reg1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        first_name = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        first_name.send_keys("Иван")
+
+        last_name = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        last_name.send_keys("Петров")
+
+        email = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        email.send_keys("test@example.com")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Registration1 test failed")
+
+        browser.quit()
+
+    def test_reg2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+
+        first_name = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+        first_name.send_keys("Иван")
+
+        last_name = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+        last_name.send_keys("Петров")
+
+        email = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+        email.send_keys("test@example.com")
+
+        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+        button.click()
+
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Registration1 test failed")
+        browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
